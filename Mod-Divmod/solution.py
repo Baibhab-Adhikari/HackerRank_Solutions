34104a3ca4c00ce74cd7eb781c711e732891a1a3
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-07-10 13:29:34 +0530

    added solution for mod divmod

---
+++ Mod-Divmod/solution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+#input two int a and b
+a = int(input())
+b = int(input())
+# printing outputs 
+print(a // b)  # integer division
+print(a % b)  # modulo division
+print(divmod(a, b))  # using divmod()
