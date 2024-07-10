465b26d47849a59a33068c3704b3a46d3ceaf7e6
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-07-10 13:29:59 +0530

    added solution for power mod power

---
+++ Power-ModPower/solution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# input integers a, b and m
+a = int(input())
+b = int(input())
+m = int(input())
+# printing results
+print(pow(a, b))  # a raised to power b
+print(pow(a, b, m))  # a raised to power b modulo division by m
