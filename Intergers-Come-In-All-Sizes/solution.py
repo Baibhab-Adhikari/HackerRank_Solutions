669636b8f857cae41910ac2a21acc44ce9353d95
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-07-10 13:29:06 +0530

    added solution for integers come in all sizes

---
+++ Intergers-Come-In-All-Sizes/solution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# input integers a, b, c, d
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+#print the result of a^b + c^d
+print((pow(a, b)) + (pow(c, d)))
