3ab55d50d57d108db6994cc9216f00558c8d2dd7
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-07-25 23:29:26 +0530

    Added solution for Polar Coordinates problem

---
+++ Polar-Coordinates/solution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+# input the complex number
+z = complex(input())
+# print the mod of z
+print(abs(z))
+# print the phi of z
+print(cmath.phase(z))
